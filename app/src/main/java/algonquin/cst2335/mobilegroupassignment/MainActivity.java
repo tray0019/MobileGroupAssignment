0f676ec7baf936018620d053a62e64c568df5fd9
tray0019 <tray0019@algonquinlive.com>
2024-03-08 15:11:51 -0500

    First Commit - For each activity and xml i've provided instruction for easier coding experience.
    Commited: Rustom Trayvilla
    Date: March 08/ 2024

---
+++ app/src/main/java/algonquin/cst2335/mobilegroupassignment/MainActivity.java
@@ -0,0 +1,55 @@
+package algonquin.cst2335.mobilegroupassignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * These is the Menu or dashboard of
+ * the application. This class provides the function for the
+ * 4 buttons, when click it launches an activity.
+ *
+ * @Author Rustom
+ * @Since March/08/2024
+ */
+public class MainActivity extends AppCompatActivity {
+
+    /**DO NOT CODE IN THis CLASS! Only code, when you link the button to your activity **/
+    /**These class provide the function to launch your class **/
+    /**Provide comment for your changes**/
+    /**Create a class for your own activity. Check RustomActivity class Example**/
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /** ~Rustom Function
+         * This fuction when click launch the button from my activity class(RustomClass)
+         * Sunrice and Sunset Look.
+         *
+         * Copy and follow this code for linking your activity
+         */
+        Button sunRiseSetButton = findViewById(R.id.RustomButton); //Make sure its your button from the xml activity
+        sunRiseSetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this,RustomClass.class);//Make sure its your class from the class activity
+                    startActivity(intent);
+            }
+
+            /********** CODE BELOW inside the onCreate ************/
+
+
+        }) ;
+
+        }
+    }
+
+
+
+
---
+++ app/src/main/java/algonquin/cst2335/mobilegroupassignment/RustomClass.java
@@ -0,0 +1,36 @@
+package algonquin.cst2335.mobilegroupassignment;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * This class is for my Sunrise & Sunset lookup application
+ * User interface to be able to enter latitude and longtitude
+ * to look up when sun will rise and set.
+ *
+ * @Rustom
+ * @Since March/08/2024
+ */
+public class RustomClass extends AppCompatActivity {
+
+    /**
+     * Your button in the main activity must be link in your class.
+     * This is where YOU CODE YOUR ACTIIVTY!
+     *
+     * Right click on 'mobilegroupassignment' folder and create your java class
+     */
+
+    /**Sunrise & Sunset lookup**/
+
+    /**This function set the view of rustom_layout xml**/
+    /**Copy this code and change the layout name to your xml layout**/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rustom_layout);
+    }
+
+
+}
