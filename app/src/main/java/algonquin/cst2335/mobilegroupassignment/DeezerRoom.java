c36025cce89f3d65b7648d65004c80cc405efaad
Farock Natanael Yandom Youmbi <yand0012@algonquinlive.com>
2024-03-29 01:08:18 -0400

    Third commit, first part done, able to display the list of songs to the user

--- app/src/main/java/algonquin/cst2335/mobilegroupassignment/DeezerRoom.java
+++ app/src/main/java/algonquin/cst2335/mobilegroupassignment/DeezerRoom.java
@@ -14,6 +14,16 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.android.application.R;
 import com.android.application.databinding.ActivityDeezerRoomBinding;
 import com.android.application.databinding.SongListBinding;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -21,7 +31,6 @@ import java.util.ArrayList;
 public class DeezerRoom extends AppCompatActivity {
 
     ActivityDeezerRoomBinding binding;
-    DeezerViewModel chatModel;
     private RecyclerView.Adapter<MyRowHolder> myAdapter;
     private ArrayList<DeezerSong> songs;
 
@@ -30,7 +39,6 @@ public class DeezerRoom extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityDeezerRoomBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        chatModel = new DeezerViewModel(); // Initialize your ViewModel
 
         binding.recycleView.setLayoutManager(new LinearLayoutManager(this));
 
@@ -60,17 +68,77 @@ public class DeezerRoom extends AppCompatActivity {
         binding.recycleView.setAdapter(myAdapter);
 
         binding.searchButton.setOnClickListener(click -> {
-            String song = binding.editText.getText().toString();
-            songs.add(new DeezerSong(song));
-            myAdapter.notifyItemInserted(songs.size() - 1);
+            // Get the artist name entered by the user
+            String artistName = binding.editText.getText().toString();
+            String url = "https://api.deezer.com/search/artist/?q=" + artistName;
+            JsonObjectRequest objectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    url,
+                    null,
+                    response -> {
+                        try {
+                            JSONArray data = response.getJSONArray("data");
+                            for (int i = 0; i < data.length(); i++) {
+                                JSONObject artistObject = data.getJSONObject(i);
+                                String serverArtistName = artistObject.getString("name");
+                                String modifiedServerArtistName = serverArtistName.toLowerCase().replaceAll("\\s", "");
+                                String modifiedArtistName = artistName.toLowerCase().replaceAll("\\s", "");
+                                int id = artistObject.getInt("id");
+                                String tracklistUrl = artistObject.getString("tracklist");
+                                // Convert the ID to a string
+                                String idString = String.valueOf(id);
+
+                                if (tracklistUrl.contains(idString) && modifiedArtistName.equals(modifiedServerArtistName)) {
+                                    JsonObjectRequest tracklistRequest = new JsonObjectRequest(
+                                            Request.Method.GET,
+                                            tracklistUrl,
+                                            null,
+                                            tracklistResponse -> {
+                                                try {
+                                                    JSONArray tracklistData = tracklistResponse.getJSONArray("data");
+                                                    for (int j = 0; j < tracklistData.length(); j++) {
+                                                        JSONObject trackObject = tracklistData.getJSONObject(j);
+                                                        String songTitle = trackObject.getString("title");
+                                                        songs.add(new DeezerSong(idString));
+                                                        songs.add(new DeezerSong(songTitle));
+                                                    }
+                                                    myAdapter.notifyDataSetChanged();
+                                                } catch (JSONException e) {
+                                                    e.printStackTrace();
+                                                }
+                                            },
+                                            error -> {
+                                                // Handle error response from tracklist URL
+                                            }
+                                    );
+                                    Volley.newRequestQueue(this).add(tracklistRequest);
+                                }
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    },
+                    error -> {
+                        // Handle error response
+                    }
+            );
+            Volley.newRequestQueue(this).add(objectRequest);
         });
     }
+
+
+
+
 
     static class MyRowHolder extends RecyclerView.ViewHolder {
         TextView songText;
 
         public MyRowHolder(@NonNull View itemView) {
             super(itemView);
+
+            itemView.setOnClickListener(clk ->{
+
+            });
             songText = itemView.findViewById(R.id.message);
         }
     }
